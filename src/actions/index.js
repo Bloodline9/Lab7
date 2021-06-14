f14042dcf2de436258e034d9778bbe9906279018
Bloodline9 <70684438+Bloodline9@users.noreply.github.com>
2021-06-15 01:19:29 +0300

    Create index.js

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const AddFriend = (user) => {
+  return {
+    type: "User_ADD",
+    payloader: `${user.name.first} ${user.name.last}`
+  };
+};
+
+export const RemoveFriend = (friend) => {
+  return {
+    type: "User_Remove",
+    payloader: friend
+  };
+};
